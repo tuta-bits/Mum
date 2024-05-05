3a6aa4080a957d23c0462d8d4bbf0e0ff9258c13
tuta-bits <tuta@angelic.com>
2024-05-05 22:59:29 +1000

    Update Add Index.html with elements Add main.js with functions to make the web page responsive Add style.css with styling for the web page Add images to beautify the web page Add customised fonts Add media file for audio and visual effects

---
+++ Assets/main.js
@@ -0,0 +1,57 @@
+let timeOut = 4000;
+let slideIndex = 0;
+let autoOn = true;
+
+autoSlides();
+
+function autoSlides() {
+    timeOut = timeOut - 20;
+
+    if (autoOn == true && timeOut < 0) {
+        showSlides();
+    }
+    setTimeout(autoSlides, 20);
+}
+
+function prevSlide() {
+
+    timeOut = 4000;
+
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex--;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    if (slideIndex == 0) {
+        slideIndex = 14
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function showSlides() {
+
+    timeOut = 4000;
+
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
